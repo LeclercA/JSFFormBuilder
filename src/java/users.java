fdfb0970c881010819659c59845a8b5833458a1e
LeclercA <alexys.leclerc@gmail.com>
2017-07-10 18:47:59 -0400

    it works!

--- src/java/users.java
+++ src/java/users.java
@@ -1,16 +1,18 @@
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  *
  * @author Alexys
  */
 @Named(value = "users")
-@Dependent
-@ApplicationScoped
+@SessionScoped
 @ManagedBean
 
 public class users {
@@ -18,11 +20,7 @@ public class users {
     private String username;
     private String email;
 
-    
-    public users(){}
-    public users(String username, String email) {
-        this.username = username;
-        this.email = email;
+    public users() {
     }
 
     public String getUsername() {
@@ -40,11 +38,29 @@ public class users {
     public void setEmail(String email) {
         this.email = email;
     }
+
     /**
      * Creates a new instance of users
      *
      * @param username
      * @param email
      */
-
+    public void createUsers() {
+        Connection con = database.getConnection();
+        String query = "INSERT INTO users VALUES (NULL, ?)";
+        try {
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setString(1, username);
+            int numberOfRows = statement.executeUpdate();
+            if (numberOfRows > 0) {
+                System.out.println("Success");
+            } else {
+                System.out.println("No success");
+            }
+        } catch (SQLException e) {
+            System.out.println(username);
+            System.out.println(email);
+            System.out.println(e.getMessage());
+        }
+    }
 }
